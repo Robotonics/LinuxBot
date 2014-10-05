0c309bc6a8de16d771381bd5bd5da53aa32661cb
root <root@a13-OLinuXino.(none)>
2014-10-05 23:03:58 +0100

    update

--- drive.py
+++ drive.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
+
+
 import os
-import sys
-import ping
 import motor
+import ping
 import radar
-import time
+import GPS
+
+
+
 
 __author__ = "David Cotterill-Drew"
 __copyright__ = "Copyright 2014, RoboTonics"
@@ -15,12 +19,34 @@ __version__ = "2.0"
 __maintainer__ = __author__
 __email__ = "roboshopz@gmail.com"
 
-def go (void):
 
-	while(1):
-		dist=ping.distance(1)
-		if dist<10:
-			motor.right(1)
-		motor.forward(1)
-			
+def forward():
+	motor.forward(1)
+
+def reverse():
+	motor.reverse(1)
+
+def right():
+	motor.right(1)
+
+def left():
+	motor.left(1)
+
+def stop():
+	motor.stop(1)
+
+options = {0: forward,
+	   1: reverse,
+	   2: right,
+	   3: left,
+	   4: stop,
+}
+while(1):
+	try:
+   		num=int(raw_input('Direction(0-4):'))
+		options[num]()
+		
+
+	except ValueError:
+    		print "Not a number"
 
